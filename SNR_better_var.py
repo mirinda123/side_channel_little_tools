28e24eac2f17485d5316e093e31db90eca880ebf
mirinda <995740977@qq.com>
2022-11-03 10:29:26 +0800

    no message

---
+++ SNR_better_var.py
@@ -0,0 +1,90 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm, trange
+
+# 这个文件也是测SNR，但是使用的方差算法貌似可以防止数值错误，试一试
+def prepare_data(url_trace, trace_name, url_data, data_name, n, loadData):
+    m = []
+    v = []
+    M2 = []
+    # trace = np.load(url_trace + r"arrPart0.npy")
+    trace = np.load(url_trace + r"arrPart0.npy")
+    # count_temp = np.zeros(300, dtype=np.int32)
+
+    # count_temp是统计每一个label的数量
+    # 这里300是冗余了
+    count_temp = np.zeros(300, dtype=np.int32)
+
+    # 初始化m和v
+    for i in loadData:
+        m[i] = np.zeros(trace.shape[1])
+        v[i] = np.zeros(trace.shape[1])
+        M2[i] = np.zeros(trace.shape[1])
+    # n是block数
+    for j in trange(n):
+        data = np.loadtxt(url_data + data_name + r"{0}.txt".format(j), delimiter=',', dtype="int")
+        # print(data)
+        trace = np.load(url_trace + trace_name + r"{0}.npy".format(j))
+
+        for count, label in enumerate(data):
+            count_temp[label] +=1
+            # 枚举，count表示第几个，从0开始，到499
+
+            # d[label].append(trace[count])
+
+            # old_mean是一个向量
+            # m[label]是一个向量
+
+            delta = trace[count] - m[label]
+
+            m[label] += delta[label] / count_temp[label]
+            # 遍历每一条数据，在线计算均值和方差 Welford's online algorithm
+            # m是均值
+            # v是方差
+            delta2 = trace[count] - m[label]
+            M2[label] += delta * delta2
+    for label in loadData:
+        v[label] = M2[label] / count_temp[label]
+
+    signal = []
+    noise = []
+    for i_ in loadData:
+        # signal_trace 存所有label的均值曲线
+        signal.append(m[i_])
+    for j_ in loadData:
+        noise.append(v[j_])
+    signal_var = np.var(signal, axis=0)
+
+    # 这里好像不太一样/
+    noise_mean = np.mean(noise, axis=0)
+    return signal_var / noise_mean
+
+
+def snr_function(n, url_trace, trace_name, url_data, data_name):
+    plt.rcParams['figure.figsize'] = (12.0, 4.0)
+    f, ax = plt.subplots(1, 1)
+    ax.set_title('snr_traces')
+    list = (np.unique(np.loadtxt(url_data + data_name + r"0.txt", delimiter=',', dtype="int"))).tolist()
+    for i in range(n - 1):
+        dataaaa = np.loadtxt(url_data + data_name + r"{0}.txt".format(i + 1), delimiter=',', dtype="int")
+        num = (np.unique(dataaaa)).tolist()
+        list = list + num
+    loadData = (np.unique(list))
+    # 得到的loadData是*所有块中*去重后的输入数据
+    result_data = prepare_data(url_trace, trace_name, url_data, data_name, n, loadData)
+    np.save(r"F:/tracexinzeng32sh8/snr_data.npy", result_data)
+    ax.plot(result_data)
+    plt.show()
+
+
+if __name__ == '__main__':
+    # 确定文件路径，名字，块数
+
+    url_trace = r"F:/tracexinzeng32sh8/"
+    url_data = r"F:/tracexinzeng32sh8/"
+
+    data_name = "aaadata"
+    trace_name = "arrPart"
+    n = 1000
+    # 运行此函数
+    snr_function(n, url_trace, trace_name, url_data, data_name)
--- covtest.py
+++ covtest.py
@@ -1,5 +1,15 @@
 import numpy as np
-
+def better_var(X_list):
+    count = 0
+    mean = 0
+    M2 = 0
+    for i in range(len(X_list)):
+        count += 1
+        delta = X_list[i] - mean
+        mean += delta / count
+        delta2 = X_list[i] - mean
+        M2 += delta * delta2
+    print(M2 / (count))
 def online_cov(X_list,Y_list):
     meanx = meany = C = n = 0
 
@@ -22,9 +32,12 @@ X = np.random.rand(10000)
 Y = np.random.rand(10000)
 #X = [1,2,4,6,8,5,6][:6]
 #Y = [3,6,2,6,9,7,8][:6]
-online_cov(X, Y)
-
+#online_cov(X, Y)
+better_var(X)
 print("numpy_result")
 
-print(np.cov(X, Y)[0][1])
+#print(np.cov(X, Y)[0][1])
+print(np.var(X))
+
+
 
