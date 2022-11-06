1bfee89e919c9b4fabf8ad9e365b20889a4663f3
mirinda123 <995740977@qq.com>
2022-11-06 19:15:30 +0800

    no message

--- ttttttt2.py
+++ ttttttt2.py
@@ -13,4 +13,19 @@ def hammingWeight(n):
 
     return c
 
-[for i in range]
+#file_path = r"F:/weixinzeng_wave_filter_trace_16bit/"
+file_path = r"F:/weixinzeng32sh8/"
+# file_path = r"D:/ChipWhisperer5_52/cw/home/portable/chipwhisperer/jupyter/courses/sca101/traces/"
+input_data_file_name = "aaadata{0}.txt"
+# input_data_file_name = "lab3_3_zhongjianzhi_chaifen{0}.txt"
+traces_file_name = "arrPart{0}.npy"
+input_data = []
+part = 60
+for i in range(part):
+    # 收集一下所有的输入数据
+    input_data += np.loadtxt(file_path + input_data_file_name.format(i), delimiter=',', dtype="int").tolist()
+
+print("此处把input_data变成hamming重量，记得恢复")
+input_data = [hammingWeight(label) for label in input_data]
+
+print(np.unique(input_data))
